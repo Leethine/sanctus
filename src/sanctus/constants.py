0e34c573528626173b89d1572b0f7fd10e59564c
Z L <zian.li.1996@outlook.com>
2023-01-11 15:44:01 +0100

    Add constants related to config

---
+++ src/sanctus/constants.py
@@ -0,0 +1,11 @@
+# constants.py
+# Defines constants
+
+DEFAULT_LOCAL_DB_DIR = "/home/Music/my_score_sanctus"
+DEFAULT_SERVER_DB_DIR = "/tmp/sanctus_db"
+
+DEFAULT_LANG = "en"
+DEFAULT_NS = "EN"
+DEFAULT_ENGRAVOR = "lilypond"
+DEFAULT_GUI = "frescobaldi"
+DEFAULT_EXTENTION = ".ly"
