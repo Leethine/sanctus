f415946e650fbb43eb7ac87d4e0b31daa9eea6ed
Z L <zian.li.1996@outlook.com>
2023-02-15 22:52:49 +0100

    Create json printer

---
+++ sanctus/tools/json_printer.py
@@ -0,0 +1,14 @@
+import json
+
+class JsonPrinter:
+  def __init__(self) -> None:
+    pass
+  
+  def printInfo(self, jobj=dict) -> str:
+    pass
+  
+  def printInfoShort(self, jobj=dict) -> str:
+    pass
+  
+  def printKeyEntry(self, jobj=dict, key='') -> str:
+    pass
