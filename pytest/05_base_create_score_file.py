9c3df190b488fd44bfe0ad2aa6fec76a0847bdec
Z L <zian.li.1996@outlook.com>
2023-02-21 11:12:14 +0100

    Update DB score

---
+++ pytest/05_base_create_score_file.py
@@ -0,0 +1,20 @@
+import os, sys
+sys.path.append('..')
+import setup_test
+
+from sanctus.db_base.db_score import Score_IO
+from sanctus.db_base.db_metadata import Metadata_IO
+
+"""
+Testing db_base/db_score.py base class
+"""
+
+# Connect to DB
+mio = Metadata_IO(setup_test.db_abs_path)
+sio = Score_IO(setup_test.db_abs_path)
+
+op13_hashcode = mio.queryByOpus("op13")[0]["Hash"]
+
+# create 15 empty files
+for i in range(15):
+  sio.createEngravingFile(op13_hashcode)
--- sanctus/db_base/db_score.py
+++ sanctus/db_base/db_score.py
@@ -1,4 +1,4 @@
-import os, sys
+import os, sys, tarfile, base64
 import sanctus.constants as constants
 from sanctus.db_base.dbutils import File_IO, TextTools
 
@@ -7,6 +7,7 @@ class Score_IO(File_IO, TextTools):
     super().__init__(db_root)
     self._SCORE_ROOT = "score/" 
     self._FILE_DIR = self._SCORE_ROOT
+    self._TARFILE_EXTENTION = ".tar.gz"
     if not self._checkDirectory():
       print("[WARNING] Composer directory invalid")
       raise("Directory invalid")
@@ -28,8 +29,7 @@ class Score_IO(File_IO, TextTools):
     else:
       return ''
 
-
-  def getListOfScript(self, hashcode: str, extension=".ly") -> list:
+  def getListOfScript(self, hashcode: str, extension='.ly') -> list:
     score_dir = self.getScoreDirAbs(hashcode)
     scorefiles = []
     if not score_dir:
@@ -44,7 +44,7 @@ class Score_IO(File_IO, TextTools):
     scorefiles = [f for f in scorefiles if extension in f]
     return scorefiles
 
-  def createNewEngravingFile(self, hashcode: str, extension='.ly') -> bool:
+  def createEngravingFile(self, hashcode: str, extension='.ly', content='% ---- ') -> bool:
     score_dir = self.getScoreDirAbs(hashcode)
 
     # dir doesn't exist, create dir and file
@@ -52,29 +52,69 @@ class Score_IO(File_IO, TextTools):
       score_dir = self._FILE_DIR + hashcode[0] + "/" + hashcode
       os.mkdir(score_dir)
       with open(score_dir + "/" + "01" + extension, "w") as f:
-        f.write("%!: ---- ")
-      print("[INFO] New engraving file created with hash = {}".format(hashcode))
+        f.write(content)
+      print("[INFO] Empty engraving file created with hash {}".format(hashcode))
       return True
-    else:
+    else:    
+      # dir path exists, create file directly
+      if os.path.exists(score_dir) and hashcode != "":
+        score_list = self.getListOfScript(hashcode)
+        if not score_list:
+          # if no score exists in directory, create new file
+          with open(score_dir + "/" + "01" + extension, "w") as f:
+            f.write(content)
+          print("[INFO] Empty engraving file created with hash {}".format(hashcode))
+          return True
+        else:
+          # if score already exist in directory, create next file
+          score_list = [fn.replace(".ly", "") for fn in score_list]
+          score_list.sort()
+          next_file = int(score_list[-1]) + 1
+          next_file = "{:0>2d}".format(next_file)
+          with open(score_dir + "/" + next_file + extension, "w") as f:
+            f.write(content)
+          print("[INFO] Empty engraving file created with hash {}".format(hashcode))
+          return True
       return False
-
-    if os.path.exists(score_dir) and hashcode != "":
-      score_list = self.getListOfScript(hashcode)
-      if not score_list:
-        # if no score exists in directory, create new file
-        with open(score_dir + "/" + "01" + extension, "w") as f:
-          f.write("%!: ---- ")
-        print("[INFO] New engraving file created with hash = {}".format(hashcode))
-        return True
-      else:
-        # if score already exist in directory, create next file
-        score_list = [fn.replace(".ly", "") for fn in score_list]
-        score_list.sort()
-        next_file = int(score_list[-1]) + 1
-        next_file = "{:0>2d}".format(next_file)
-        with open(score_dir + "/" + next_file + extension, "w") as f:
-          f.write("%!: ---- ")
-        print("[INFO] New engraving file created with hash = {}".format(hashcode))
-        return True
     
     return False
+  
+  def packScoreDir(self, hashcode: str, extension='.ly') -> str:
+    scorepath = self.getScoreDirAbs(hashcode)
+    if not scorepath:
+      return ''
+    else:
+      tarpath = scorepath + "/" + hashcode + self._TARFILE_EXTENTION
+      if os.path.exists(tarpath):
+        os.remove(tarpath)
+      with tarfile.open(tarpath, mode='x:gz') as tar:
+        for fname in self.getListOfScript(hashcode, extension):
+          tar.add(scorepath + "/" + fname)
+      return tarpath
+  
+  def getScorePackageAsRaw(self, hashcode: str, extension='.ly') -> bytes:
+    scorepath = self.getScoreDirAbs(hashcode)
+    tarpath = scorepath + "/" + hashcode + self._TARFILE_EXTENTION
+    if os.path.exists(tarpath):
+      with open(tarpath, "rb") as tar:
+        return base64.b64encode(tar.read())
+    else:
+      return b""
+
+  def unpackScoreDir(self, hashcode: str, extension='.ly') -> bool:
+    scorepath = self.getScoreDirAbs(hashcode)
+    tarpath = scorepath + "/" + hashcode + self._TARFILE_EXTENTION
+    if not os.path.exists(tarpath):
+      print("[Warning] <unpackScoreDir> tarball does not exist")
+      return False
+    else:
+      for fname in self.getListOfScript(hashcode, extension):
+        os.remove(scorepath + "/" + fname)
+      with tarfile.open(tarpath, mode='r:gz') as tar:
+        for member in tar.getmembers():
+          if member.isreg():
+            member.name = os.path.basename(member.name)
+            tar.extract(member, path=scorepath)
+        print("[INFO] Extracted {} files".format(len(tar.getmembers())))
+      return True
+
