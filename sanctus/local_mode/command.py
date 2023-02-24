307d9f6c2c73c27ff592b16842fdfd5bc36c9032
Z L <zian.li.1996@outlook.com>
2023-02-24 17:08:21 +0100

    Create empty methods (to be implemented) in local-mode command line interface

--- sanctus/local_mode/command.py
+++ sanctus/local_mode/command.py
@@ -4,20 +4,32 @@ from sanctus.db_base.cli_adapter import DataBaseCliAdapter
 from constants import DEFAULT_LOCAL_DB_DIR
 
 """
-find composer [fmlyname, abbrname, year]
-find piece/arrangement/collection/template [title, opus, composer]
+List of commands:
 
-add composer
-add piece/collection/arrangement/template
+fc: find composer [fmlyname, abbrname, year]
+fp: find piece/arrangement/collection/template [title, opus, composer]
 
-rm composer
-rm piece/arrangement/collection/template
+nc: create new composer
+nw: create new piece/collection/arrangement/template
+nwp: create new piece
+nwc: create new collection
+nwt: create new template
 
-update composer
-update piece/arrangement/collection/template
+apc: add piece to collection
 
-ls composer
-ls piece/collection/arrangement/template/all
+rc: remove composer
+rw: remove piece/arrangement/collection/template
+
+uc: update composer
+uw: update piece/arrangement/collection/template
+
+lc: list composer
+lw: list all piece/collection/arrangement/template
+lwp: list pieces
+lwc: list collections
+lwa: list arrangements
+lwt: list templates
+
 """
 
 class CommandLine():
@@ -31,12 +43,77 @@ class CommandLine():
       elif len(candidates) == 1:
         return candidates[0]
       else:
-        selected = input("Select an item [1-"+str(len(candidates))+"]: ")
-        selected = int(selected.replace(" ","")) - 1
-        return candidates[selected]
+        while True:
+          selected = input("Select an item [1-"+str(len(candidates))+"]: ")
+          selected = int(selected.replace(" ","")) - 1
+          if selected < len(candidates):
+            return candidates[selected]
+          else:
+            print(str(selected) + " is out of range, please select again!")
     except:
       print("[Error] <_processSelectedNumber> cannot process")
+      return {}
+  
+  # Commands
+  def __fc(self, ):
+    pass
+  
+  def __fw(self, ):
+    pass
+  
+  def __nc(self, ):
+    pass
+  
+  def __nwp(self, ):
+    pass
+  
+  def __nwc(self, ):
+    pass
+  
+  def __nwa(self, ):
+    pass
+  
+  def __nwt(self, ):
+    pass
+  
+  def __nw(self, ):
+    pass
+  
+  def __apc(self, ):
+    pass
+  
+  def __rc(self, ):
+    pass
+  
+  def __rw(self, ):
+    pass
+  
+  def __uc(self, ):
+    pass
+  
+  def __uw(self, ):
+    pass
+  
+  def __lc(self, ):
+    pass
+  
+  def __lwp(self, ):
+    pass
+  
+  def __lwc(self, ):
+    pass
+  
+  def __lwa(self, ):
+    pass
+  
+  def __lwt(self, ):
+    pass
+  
+  def __lw(self, ):
+    pass
 
+  # End Commands
+  
   def run(self) -> int:
     pass
 
